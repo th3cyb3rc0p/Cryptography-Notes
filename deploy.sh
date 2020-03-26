db87c147a5cc0c72a9723cf6aa81c96f17aca976
z-r0crypt <tripathi313@gmail.com>
2020-03-26 08:10:51 -0700

    rebuilding site Thu Mar 26 08:10:51 PDT 2020

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
